30020682f11ce7ccd74c2935a0ebbf3c14f5c63d
Marcos Romagnoli <marcos.romagnoli84@gmail.com>
2024-04-09 13:45:59 -0300

    Primer commit

---
+++ estadisticas/__init__.py
@@ -0,0 +1,34 @@
+
+def generate_statistics(names_list, goals, goals_avoided, assists):
+    combined_data = zip(names_list, goals, goals_avoided, assists)
+    statistics = list(map(lambda data: {
+        "Name": data[0],
+        "Goals": data[1],
+        "Avoided goals": data[2],
+        "Assists": data[3]
+    }, combined_data))
+    
+    return statistics
+
+
+def get_top_scorer(statistics):
+    max_goals = max(statistics, key=lambda x: x["Goals"])
+    return max_goals["Name"], max_goals["Goals"]
+
+
+def calculate_score(player):
+    return player["Goals"] * 1.5 + player["Goals"] * 1.25 + player["Assists"]
+
+def most_influential_player(statistics):
+    most_influential_player = max(statistics, key=calculate_score)
+    return most_influential_player["Name"]
+
+
+def average_goals_per_match_team(statistics):
+    total_goals = sum(player["Goals"] for player in statistics)
+    return total_goals / 25
+
+
+def average_goals_per_match_top_scorer(statistics):
+    top_scorer = max(statistics, key=lambda x: x["Goals"])
+    return top_scorer["Goals"] / 25
